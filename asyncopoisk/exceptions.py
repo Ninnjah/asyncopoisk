4c1e9a87bdb0fc965e7807efdd42b8d911110a81
Ninnjah <yand.man4@gmail.com>
2024-01-11 09:15:41 +0300

    Make exception message works

--- asyncopoisk/exceptions.py
+++ asyncopoisk/exceptions.py
@@ -1,10 +1,12 @@
+from httpx import Response
+
+
 class KinopoiskException(Exception):
-    pass
+    def __init__(self, message: str = ""):
+        super().__init__(message)
 
 
 class BadRequest(KinopoiskException):
-    def __init__(self, request):
-        self.request = request
-
-    def __repr__(self) -> str:
-        return f"{type(self).__name__}('{self}')"
+    def __init__(self, response: Response):
+        self.message = f"{type(self).__name__}('{self}') - {response.content}"
+        super().__init__(self.message)
